ec101adf10a8dbc67708ef7987229af65426219c
Victorialalaa <victorkedu@gmail.com>
2021-10-27 19:18:42 -1000

    added login signup and welcome screen setups

---
+++ screens/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+} from './../components/styles';
+
+const Login = () => {
+    return (
+        <StyledContainer>
+            <InnerContainer>
+                <PageTitle>f*ck cassid</PageTitle>
+            </InnerContainer>
+        </StyledContainer>
+    );
+}
+
+export default Login;
