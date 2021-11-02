cfca945ea6b94f23917e5e97858e24bae15665ac
Victorialalaa <victorkedu@gmail.com>
2021-11-02 11:44:59 -1000

    added slider package and other code related

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import RootStack from './navigators/RootStack';
+//import chemicalLevel from './screens/chemicalLevel';
 
 export default function App() {
   return <RootStack />;
--- screens/chemicalLevel.js
+++ screens/chemicalLevel.js
@@ -1,2 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Slider from '@react-native-community/slider';
 
+export default function chemicalLevel() {
+    const [range, setRange] = useState('50%');
+    const [sliding, setSliding] = useState('Inactive');
+
+    return (
+        <View style={styles.container}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold'}}>{range}</Text>
+        <Text style={{ fontSize: 20, fontWeight: 'bold'}}>{sliding}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 
