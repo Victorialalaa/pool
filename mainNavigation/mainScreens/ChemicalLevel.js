85f51eb555fa7d80166b65a7435995a72448ddf3
Victorialalaa <victorkedu@gmail.com>
2021-11-04 12:46:27 -1000

    added somechemical bars back lol

--- mainNavigation/mainScreens/ChemicalLevel.js
+++ mainNavigation/mainScreens/ChemicalLevel.js
@@ -4,12 +4,44 @@ import { StyleSheet, Text, View, ScrollView } from "react-native";
 import Slider from "@react-native-community/slider";
 
 export default function ChemicalLevel() {
-  const [range, setRange] = useState("50%");//js set independently in future
+  const [range, setRange] = useState("50%");
   const [sliding, setSliding] = useState("Inactive");
 
   return (
     <View style={styles.container}>
       <ScrollView>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>{range}</Text>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>{sliding}</Text>
+
+        <Slider
+          style={{ width: 250, height: 40 }}
+          minimumValue={0}
+          maximumValue={1}
+          minimumTrackTintColor="tomato"
+          maximumTrackTintColor="#000"
+          thumbTintColor="tomato"
+          value={0.5}
+          onValueChange={(value) => setRange(parseInt(value * 100) + "%")}
+          onSlidingStart={() => setSliding("Sliding")}
+          onSlidingComplete={() => setSliding("Inactive")}
+        />
+
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>{range}</Text>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>{sliding}</Text>
+
+        <Slider
+          style={{ width: 250, height: 40 }}
+          minimumValue={0}
+          maximumValue={1}
+          minimumTrackTintColor="tomato"
+          maximumTrackTintColor="#000"
+          thumbTintColor="tomato"
+          value={0.5}
+          onValueChange={(value) => setRange(parseInt(value * 100) + "%")}
+          onSlidingStart={() => setSliding("Sliding")}
+          onSlidingComplete={() => setSliding("Inactive")}
+        />
+
         <Text style={{ fontSize: 20, fontWeight: "bold" }}>{range}</Text>
         <Text style={{ fontSize: 20, fontWeight: "bold" }}>{sliding}</Text>
 
